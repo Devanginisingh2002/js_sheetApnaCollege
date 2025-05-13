46bdedcc338bb6b39a18e23dcd5c19411809546b
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-05-13 18:27:11 +0530

    javaScr

---
+++ script2.js
@@ -0,0 +1,114 @@
+/******** Extra JS QUESTION *********/
+
+// Make a trafic light
+/* let color = "yellow";
+if (color === "red") {
+    console.log("stop");
+} else if (color === "yellow") {
+    console.log("Ready to go");
+} else if (color === "green") {
+    console.log("Go");
+} else{
+    console.log("No Color");
+}
+*/
+
+// Good String: starts with letter 'a' & length > 3.
+/* 
+let check = "dcba";
+if (check[0] === "a" && check.length > 3) {
+    console.log("It's a String");
+} else {
+    console.log("Not a string");
+}
+*/
+/* OUTPUT of CODE: 
+let num = 12;
+if ((num%3 === 0) && ( (num+1 == 15) || (num-1 == 11 ))){
+   console.log("Safe");
+}else{
+   console.log("Unsafe");
+}
+*/
+
+/****** JS PART 2: PRACTICE QUESTION ******/
+
+/* Q1. Number divided by 10 or not?
+ let num = 23;
+if(num % 10 == 0) {
+    console.log("Good");
+} else {
+    console.log("Bad");
+}
+*/
+
+/* Q2. Take input name & age print using template literals.
+let name_1 = prompt("Input your name: ")
+let age = prompt("Input your Age: ")
+alert(`${name_1} is ${age} years old`)
+*/
+
+/* Switch statement question 
+let month = 10;
+switch(month){
+    case 1: {
+        console.log("January, Febr, March");
+    }
+    break;
+    case 2:{
+        console.log("April, May, June");
+    }
+    break;
+    case 3: {
+        console.log("July, August, September");
+    }
+    break;
+    case 4: {
+        console.log("October, November, December");
+    }
+    break;
+    
+    default:{
+        console.log("Break");
+    }
+}
+*/
+
+/* Q4. Check string if first letter is 'A' or 'a' and string length > 5.
+let string1 = "abcedede"
+if(string1[0] == 'A' || string1[0] == 'a' && (string1.length > 5)){
+    console.log("Golden");
+} else {
+    console.log("Stop")
+}
+*/
+
+/* Q5. Largest of 3 numbers. 
+let a = 10;
+let b = 20;
+let c = 50;
+if(a>b){
+    console.log(a)
+} else if(a>c){
+    console.log(a)
+} else if(b>c) {
+    console.log(b)
+} else{
+    console.log(c)
+}
+*/
+
+/* Q6. Check if 2 numbers have the same last digit.
+
+let num1 = parseInt(prompt("Enter your num1: "));
+let num2 = parseInt(prompt("Enter your num1: "));
+
+if (num1 % 10 == num2 % 10) {
+    console.log("Number are same");
+} else {
+    console.log("Number are not same");
+}
+*/
+
+
+
