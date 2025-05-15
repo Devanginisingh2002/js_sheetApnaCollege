381cc99a26a604747161405b9085841a448a395c
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-05-15 14:23:12 +0530

    file

---
+++ script3.js
@@ -0,0 +1,121 @@
+/***** STRING METHOD ******/
+/*
+// 1. Trim Method
+let check = "            hello";
+console.log(check.trim());
+let password = prompt("Set Password")
+console.log(password.trim());
+*/
+
+/*
+// 2. Upper & Lower Case
+let check2 = "hello"
+let check3 = "I AM A SOFTware engineer"
+console.log(check2.toUpperCase())
+console.log(check3.toLowerCase())
+*/
+
+/*
+// 3. IndexOf
+let check = "I Am Learner check"
+console.log(check.indexOf("Am"))
+*/
+
+/*
+// Chaining Method
+let string1 = 'i am learner, doing coding    '
+console.log(string1.trim().toUpperCase())
+*/
+
+/*
+ // Slice Method
+ let str = 'ILoveCoding'
+ console.log(str.slice(5))   
+ console.log(str.slice(0, 5))
+ let check = 'Ilove'
+ console.log("Third: " + check.slice(-4, -2)) // lo (explanation: startI = -4, endI = -2 (last index is not included))
+ */
+
+/*
+ // Replace & Repeat Method
+
+ // Replace
+ let string1 = "IloveCoding"
+ let ans = string1.replace("love", "check")
+ console.log(ans)
+
+ // Repeat
+ let x = 'code'
+ console.log(x.repeat(10)) 
+*/
+
+/* Arrays
+ * Arrays Mehthod
+ // Push
+ let arr = ['aman', 'radha', 'krishna']
+ console.log(`Old arr: ${arr}`)
+ let ans = arr.push('valmiki')
+ console.log(ans)
+ console.log(`New arr: ${arr}`)
+ 
+  //Pop
+  let arr1 = ['aman', 'radha', 'krishna']
+ console.log(`Old arr: ${arr1}`)
+ let ans = arr1.pop()
+ console.log(ans)
+ console.log(`New arr: ${arr1}`)
+ 
+ //Unshift
+ let arr = ['aman', 'radha', 'krishna']
+ console.log(`Old arr: ${arr}`)
+ let ans = arr.unshift('valmiki')
+ console.log(ans)
+ console.log(`New arr: ${arr}`)
+ //shift
+ let arr = ['aman', 'radha', 'krishna']
+ console.log(`Old arr: ${arr}`)
+ let ans = arr.shift()
+ console.log(ans)
+ console.log(`New arr: ${arr}`)
+
+ // indexOf
+ let arr = ["a", "b", "c", "d"]
+ console.log(arr.indexOf("b"))
+ 
+ //includes
+ let arr1 = ["alex", "brave", "delusion", "hope"]
+ console.log(arr1.includes("delusion"))
+ 
+  //concat
+  let arr1 = ["al", "bl", "cl", "dl"]
+  let arr2 = ["el", "fl", "gl", "hl"]
+ 
+  let check = arr1.concat(arr2)
+  console.log(check)
+
+  // Reverse
+  let arr = [1, 2, 3, 4]
+  console.log(arr.reverse())
+  
+ */
+
+
+
+
+
+
+/***** ASSIGNMENT JS(Part 3) *****/
+/* 
+let arr = [7,9,0,-2]
+let num = 3
+console.log("First n elements: ", arr.slice(0, num));
+*/
+
+/* 
+let arr2 = [7, 9, 0, -2]
+
+let num = 3
+console.log("Last n elements: ", arr2.slice(arr2.length-num));
+*/
+// let arr = [[2, 3], [{ name: "key" }]];
+// console.log(typeof arr);
