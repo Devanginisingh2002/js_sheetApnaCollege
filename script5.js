cfe63cde5a2d5ab89506a0dd59be1675648ed06b
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-05-18 08:52:28 +0530

    js_5

---
+++ script5.js
@@ -0,0 +1,50 @@
+"Object & Object Literals";
+
+/* Ex:1: Accessing the object 
+let student = {
+    name: "Dev",
+    class: "Standard",
+    Number: 808170,
+    email: null
+};
+console.log(student.name)
+console.log(student["class"])
+console.log(student["Number"])
+console.log(student.email)
+*/
+
+/*Ex:2 Accessing the array & array of array
+let post = {
+    name: "riya",
+    list: ["xyz", "abc"],
+    check: [
+        ["alex", "router"],
+        ["bob"]
+    ]
+};
+console.log(post.list);
+console.log(post["list"]);
+console.log(post["list"][0])
+console.log(post.list[1])
+console.log(post.check[0][1])
+console.log(post["check"][1])
+*/
+
+/*Ex: 3 Learning accessing the values
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
+}
+// console.log(obj.1)      ❌
+// console.log(obj[1])     ✅
+// console.log(obj[true])  ✅
+// console.log(obj[null])  ✅
+// console.log(obj.null)   ✅
+// console.log(obj.undefined)  ✅
+*/
+
+/* */
