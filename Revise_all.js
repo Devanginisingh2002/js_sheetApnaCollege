497f081232df16774e964e0ffeeb6d6903ac620d
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-07-12 12:01:30 +0530

    random ques

---
+++ Revise_all.js
@@ -0,0 +1,25 @@
+'Random Question Practice'
+'Q1:'
+// let str = prompt("Enter your number: ");
+// if(str[0] == 'A' || str[0] == 'a' && str.length >= 5){
+//     console.log("Golden");
+// } else {
+//     console.log("Not");
+// }
+
+'Q2:'
+// let a = 2000;
+// let b = 110;
+// let c = 90;
+// if(a>b){
+//     console.log(a);
+// } else if(a>c){
+//     console.log(a);
+// } else if(b>c){
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+'Q3:'
+
